590e419dba78a9b1d055a4eca64a89ecc0f7f01b
Dmytro Kolomatsky <dkolomatsky@gmail.com>
2024-06-03 07:45:19 +0200

    created ability to submit the data to the backend

--- src/components/CheckOut.jsx
+++ src/components/CheckOut.jsx
@@ -19,12 +19,37 @@ export default function CheckOut() {
   function handleClose() {
     userProgressCtx.hideCheckOut();
   }
+  // метод для отправки данных(формы) на сервер
+  function handleSubmit(event) {
+    event.preventDefault(); // запрещаем браузеру самостоятельно отправлять запросы
+    /*
+    1. в каждом поле input устанвмть обработичик onChange={} и работать с состоянием 
+    2. можно в инпутах использовать ref и через forwardRef получать значения по ссылке
+    3. ✅❗а можно через встроеный метод new FormData
+    */
+    const fd = new FormData(event.target); // получаем данные из формы используя встроеный обьект FormData и передаем в него форму target - это наша форма
+    const customerData = Object.fromEntries(fd.entries()); // ❗✅ используя Object.fromEntries можно преобразовать обьект fd в более простой JavaScript обьект где инпуты будут означать свойства а введенные данные будут их знчаениями типа вот так {email : dkolomatsky@gmail.com}
+
+    fetch("http://localhost:3000/orders", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // в тело передаем данные в формате JSON в той структуре которую требует настройки бекенда (обьект в котором свойство order со значением вложеного обьекта со свойствами items со значениями получеными из контекста, customer со значениями получеными из формы)
+      body: JSON.stringify({
+        order: {
+          items: cartCtx.items,
+          customer: customerData,
+        },
+      }),
+    });
+  }
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>CheckOut</h2>
         <p>Total amount: {currencyFormatter.format(totalCartPrice)}</p>
-        <Input type="text" label="Full name" id="full-name" />
+        <Input type="text" label="Full name" id="name" />
         <Input type="email" label="Email address" id="email" />
         <Input type="text" label="Street" id="street" />
         <div className="control-row">
